5e83357bdc20d70b1b2ae1df054c54e109dc8e84
vivek-12001 <2017.vivek.choudhary@ves.ac.in>
2021-10-04 12:33:15 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def hello_world(request):
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return f'Hello i am Vivek Choudhary !!'
